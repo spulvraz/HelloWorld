d7c9eada1a28e5e92db2eaef1e76c3c40e0374ad
Franka <frances.bertoncelli@gmail.com>
2016-05-29 12:50:51 +0200

    first changement

--- Bomba/Controllers/HomeController.cs
+++ Bomba/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Bomba.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "First change";
 
             return View();
         }
